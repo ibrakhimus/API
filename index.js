e03357263c27f9e09adbd762c272c48c6736e05b
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-01-21 09:00:45 -0500

    async await users

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main() {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users")
+    const userData = await users.json()
+    console.log(userData)
+}
+
+main()
