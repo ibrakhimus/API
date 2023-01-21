0441f88791b47121759ed7cb448ec607140aeb3f
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-01-21 09:06:55 -0500

    html

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users")
     const userData = await users.json()
     console.log(userData)
+
+    const cardsHTML = books.map((book) => {
+       return `<div class="user-card">
+            <div class="user-card__container">
+                <h3>User's Name</h4>
+                <p><b>Email:</b> email@email.com</p>
+                <p><b>Phone:</b> 0000000000</p>
+                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+            </div>
+        </div>`
+    })
 }
 
 main()
