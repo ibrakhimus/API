49a39af9f1077e1699a1b06c18f6536923493174
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-01-21 09:23:09 -0500

    fetching api data

--- index.js
+++ index.js
@@ -2,20 +2,21 @@
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
 async function main() {
-    const users = await fetch("https://jsonplaceholder.typicode.com/users")
-    const userData = await users.json()
-    console.log(userData)
-
-    const cardsHTML = books.map((book) => {
-       return `<div class="user-card">
-            <div class="user-card__container">
-                <h3>User's Name</h4>
-                <p><b>Email:</b> email@email.com</p>
-                <p><b>Phone:</b> 0000000000</p>
-                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-            </div>
-        </div>`
-    })
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await users.json();
+    const userListEl = document.querySelector(".user-list");
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
 
-main()
+main();
+
+function userHTML(user) {
+    return `<div class="user-card">
+    <div class="user-card__container">
+        <h3>${user.name}</h4>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+    </div>
+</div>`
+}
