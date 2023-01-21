fb894497d682c7a0ed81513d2ddac503d9218d53
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-01-21 09:33:25 -0500

    onclick

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list");
 
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
     const usersData = await users.json();
-    const userListEl = document.querySelector(".user-list");
     userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
 
 main();
 
+function showUserPosts(user) {
+    console.log(user);
+}
+
 function userHTML(user) {
-    return `<div class="user-card">
+    return `<div class="user-card" onclick = "showUserPosts(${user})">
     <div class="user-card__container">
         <h3>${user.name}</h4>
         <p><b>Email:</b> ${user.email}</p>
